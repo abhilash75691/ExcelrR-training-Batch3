334d3fd7b8d5b9bd70daf8b83df7bf69968779a0
abhilash75691 <kallapuabhilash00589@gmail.com>
2025-01-09 12:51:18 +0530

    FizzBizz

--- Demo020.java
+++ Demo020.java
@@ -1,25 +1,32 @@
-public class Demo020 {
+import java.util.Scanner;
+
+public class Demo020 {  
 
     public static String getFizzBizz(int num) {
         if (num <= 0) {
             return "Error";
-        } else if (num % 3 == 0 && num % 5 == 0) {
-            return "FIZZBIZZ";
-        } else if (num % 3 == 0) {
-            return "FIZZ";
-        } else if (num % 5 == 0) {
-            return "BIZZ";
-        } else {
-            return Integer.toString(num);
         }
+
+        if (num % 3 == 0 && num % 5 == 0) {
+            return "FIZZBIZZ";
+        }
+        if (num % 3 == 0) {
+            return "FIZZ";
+        }
+        if (num % 5 == 0) {
+            return "BIZZ";
+        }
+
+        return String.valueOf(num);
     }
 
     public static void main(String[] args) {
-        System.out.println(getFizzBizz(33));
-        System.out.println(getFizzBizz(5));
-        System.out.println(getFizzBizz(15));
-        System.out.println(getFizzBizz(7));
-        System.out.println(getFizzBizz(-1));
-        System.out.println(getFizzBizz(0));
-    }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int input = scanner.nextInt();
+
+        System.out.println(getFizzBizz(input));
+        scanner.close();
+}
 }
